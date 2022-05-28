f3e7d2e826db1f60fcfa87066ca145354d264a8a
algorithsmist <105970538+beaksfl@users.noreply.github.com>
2022-05-28 23:31:10 +0900

    220528

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,28 @@
-API_KEY = document.querySelector("#api_key");
 function onGeoOk(position) {
+  API_KEY = api_key;
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  setApi();
   fetch(url).then((response) =>
     response.json().then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#wehater span:last-child");
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main}/ ${data.main.temp}`;
-      setAndHide();
     })
   );
 }
 function onGeoError() {
-  const newKey = document.createElement("input");
   alert("Can't find your location");
 }
 
-function setAndHide() {
-  localStorage.setItem("api_key", API_KEY);
-  api_key.classList.add("hidden");
+function setApi() {
+  if (api_key === null) {
+    var api_key = prompt("Enter your API key");
+    localStorage.setItem("api_key", API_KEY);
+  } else {
+    localStorage.getItem("api_key");
+  }
 }
-//navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
