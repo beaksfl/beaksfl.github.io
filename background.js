aec0760ec171d451dd5336ee3d752c6a1488c2f4
beaksfl <105970538+beasfl@users.noreply.github.com>
2022-06-11 21:20:08 +0900

    220611

--- background.js
+++
@@ -1,8 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
-
-const chosenImages = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-
-bgImage.src = `images/${chosenImages}`;
-
-document.body.appendChild(bgImage);
